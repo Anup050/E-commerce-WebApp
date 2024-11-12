2a3e2439e28f636513bc6f685db5faff2dec8bc8
Ankit420H <2050yadavankit@gmail.com>
2024-11-12 20:05:42 +0530

    initial commit

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
+
+const About = () => {
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Welcome to Inside Fashion – your ultimate gateway to the latest
+            trends, timeless styles, and fashion inspiration! At Inside Fashion,
+            we believe that style is more than just clothing; it’s a personal
+            expression and a reflection of culture, creativity, and
+            individuality.
+          </p>
+          <p>
+            Whether you’re a fashion aficionado or simply looking to stay
+            updated, Inside Fashion has something for you. Explore with us, stay
+            ahead of trends, and let’s redefine what fashion means – together.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Our mission is to make fashion accessible and inspiring for
+            everyone. We bring you a curated selection of styles, from emerging
+            designers to iconic brands, covering everything from casual wear to
+            haute couture. Inside Fashion is committed to offering insights into
+            the ever-evolving fashion world, along with tips, guides, and ideas
+            to help you find and embrace your unique style.
+          </p>
+        </div>
+      </div>
+
+      <div className=" text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className=" text-gray-600">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className=" text-gray-600">
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className=" text-gray-600">
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
+  );
+};
+
+export default About;
